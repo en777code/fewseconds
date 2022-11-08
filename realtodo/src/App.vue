99e9f7a2265f4a221465870fe29fad3ce9c2e9e9
en777code <gam912065@gmail.com>
2022-11-08 19:42:52 +0900

    f

--- realtodo/src/App.vue
+++ realtodo/src/App.vue
@@ -6,6 +6,7 @@
       <div class="pa-3">
         <router-view></router-view>
       </div>
+      <button>@</button>
     </v-main>
   </v-app>
    
@@ -16,7 +17,19 @@
 <script>
 import HeaderMenu from "./menu/HeaderMenu.vue"
 import SideMenu from "./menu/SideMenu.vue"
+import {initializeApp} from "firebase/app"
+import {getAuth} from "firebase/auth"
 
+const app =initializeApp({
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain:process.env.VUE_APP_authDomain,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId:process.env.VUE_APP_meddagingSenderId,
+  appId:process.env.VUE_APP_appId,
+});
+
+const auth = getAuth(app)
 
 
 export default{
@@ -30,7 +43,8 @@ export default{
     SideMenu:SideMenu,
   },
   data:()=>({
-    drawer:false
+    drawer:false,
+    auth:auth,
   })
 }
 
